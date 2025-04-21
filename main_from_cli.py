c727bfaaac6af9bfa5200c487cc916d7151e329e
JoaoVit0r <joao@upoutsourcing.com>
2025-04-21 15:05:03 +0100

    refactor: uncomment FS initialization in AGNRoutines for strainingset processing

--- main_from_cli.py
+++ main_from_cli.py
@@ -1734,8 +1734,8 @@ class AGNRoutines:
                 strainingset = AGNRoutines.make_temporal_training_set(recoveredagn, targetindex, is_periodic)
             else:
                 strainingset = AGNRoutines.make_steady_state_training_set(recoveredagn, targetindex)
+            fs = FS(strainingset, recoveredagn.get_quantization(), recoveredagn.get_quantization(), type_entropy, alpha, beta, q_entropy, resultsetsize)
             sum_of_squares(10**int(os.getenv("COMPLEXITY", "6")))
-            # fs = FS(strainingset, recoveredagn.get_quantization(), recoveredagn.get_quantization(), type_entropy, alpha, beta, q_entropy, resultsetsize)
             # if not CNMeasurements.has_variation(strainingset, is_periodic):
             #     if targetaspredictors:
             #         message = f"Predictor {targetindex} name {recoveredagn.get_genes()[targetindex].get_name()}, has no variation on its values."
--- main_from_cli_no_performing.py
+++ main_from_cli_no_performing.py
@@ -1723,8 +1723,8 @@ class AGNRoutines:
                 strainingset = AGNRoutines.make_temporal_training_set(recoveredagn, targetindex, is_periodic)
             else:
                 strainingset = AGNRoutines.make_steady_state_training_set(recoveredagn, targetindex)
+            fs = FS(strainingset, recoveredagn.get_quantization(), recoveredagn.get_quantization(), type_entropy, alpha, beta, q_entropy, resultsetsize)
             sum_of_squares(10**int(os.getenv("COMPLEXITY", "6")))
-            # fs = FS(strainingset, recoveredagn.get_quantization(), recoveredagn.get_quantization(), type_entropy, alpha, beta, q_entropy, resultsetsize)
             # if not CNMeasurements.has_variation(strainingset, is_periodic):
             #     if targetaspredictors:
             #         IOFile.print_and_log(f"Predictor {targetindex} name {recoveredagn.get_genes()[targetindex].get_name()}, has no variation on its values.")
