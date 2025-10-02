866d90feea18c4635ed3d1da694343f15f47b0b6
JoaoVit0r <joao_cunha_16@outlook.com>
2025-10-02 17:17:28 -0300

    fix(wip evaluate script): time on vm1

--- evaluate_runnin_vm1.sh
+++ evaluate_runnin_vm1.sh
@@ -23,41 +23,42 @@ git status
 
 
 # Run VM1 Geneci (+ metrics)
-MONITORING_FOLDER="monitoring_plots/20250912_mutiples_runs"
+MONITORING_FOLDER="monitoring_plots/20250914_mutiples_runs"
 # mkdir -p $MONITORING_FOLDER
 # cp -r -t $MONITORING_FOLDER monitoring_plots/20250912_190348 monitoring_plots/20250912_200940
 
 python scripts/geneci_evaluate.py \
     --monitoring-dir "$MONITORING_FOLDER" \
     --external-projects "$GENECI_FOLDER" \
-    --threshold 0.000001 \
+    --threshold 0.000000001 \
     --output $MONITORING_FOLDER/evaluation_results_confident-in-0.csv \
-    --output-dir $MONITORING_FOLDER/
+    --output-dir $MONITORING_FOLDER/ \
+    --skip-binarize
 
-python scripts/geneci_evaluate.py \
-    --monitoring-dir "$MONITORING_FOLDER" \
-    --external-projects "$GENECI_FOLDER" \
-    --threshold 0.7 \
-    --output $MONITORING_FOLDER/evaluation_results_confident-in-70.csv \
-    --output-dir $MONITORING_FOLDER/
+# python scripts/geneci_evaluate.py \
+#     --monitoring-dir "$MONITORING_FOLDER" \
+#     --external-projects "$GENECI_FOLDER" \
+#     --threshold 0.7 \
+#     --output $MONITORING_FOLDER/evaluation_results_confident-in-70.csv \
+#     --output-dir $MONITORING_FOLDER/
 
-python scripts/geneci_evaluate.py \
-    --monitoring-dir "$MONITORING_FOLDER" \
-    --external-projects "$GENECI_FOLDER" \
-    --threshold 0.4 \
-    --output $MONITORING_FOLDER/evaluation_results_confident-in-40.csv \
-    --output-dir $MONITORING_FOLDER/
+# python scripts/geneci_evaluate.py \
+#     --monitoring-dir "$MONITORING_FOLDER" \
+#     --external-projects "$GENECI_FOLDER" \
+#     --threshold 0.4 \
+#     --output $MONITORING_FOLDER/evaluation_results_confident-in-40.csv \
+#     --output-dir $MONITORING_FOLDER/
 
-python scripts/geneci_evaluate.py \
-    --monitoring-dir "$MONITORING_FOLDER" \
-    --external-projects "$GENECI_FOLDER" \
-    --threshold 0.3 \
-    --output $MONITORING_FOLDER/evaluation_results_confident-in-30.csv \
-    --output-dir $MONITORING_FOLDER/
+# python scripts/geneci_evaluate.py \
+#     --monitoring-dir "$MONITORING_FOLDER" \
+#     --external-projects "$GENECI_FOLDER" \
+#     --threshold 0.3 \
+#     --output $MONITORING_FOLDER/evaluation_results_confident-in-30.csv \
+#     --output-dir $MONITORING_FOLDER/
 
-python scripts/geneci_evaluate.py \
-    --monitoring-dir "$MONITORING_FOLDER" \
-    --external-projects "$GENECI_FOLDER" \
-    --threshold 1.0 \
-    --output $MONITORING_FOLDER/evaluation_results_confident-in-100.csv \
-    --output-dir $MONITORING_FOLDER/
+# python scripts/geneci_evaluate.py \
+#     --monitoring-dir "$MONITORING_FOLDER" \
+#     --external-projects "$GENECI_FOLDER" \
+#     --threshold 1.0 \
+#     --output $MONITORING_FOLDER/evaluation_results_confident-in-100.csv \
+#     --output-dir $MONITORING_FOLDER/
--- evaluate_runnin_vm2_toRunVM1data.sh
+++ evaluate_runnin_vm2_toRunVM1data.sh
@@ -57,27 +57,27 @@ mkdir -p $MONITORING_FOLDER/matlab/results
 # echo manual edit the go_my to use this path to predictions!!!
 # exit;
 
-# List of methods
-methods=("GENIE3_ET" "GENIE3_RF" "KBOOST")
+# # List of methods
+# methods=("GENIE3_ET" "GENIE3_RF" "KBOOST")
 
-for method in "${methods[@]}"; do
-    # method_lower=$(echo "$method" | tr '[:upper:]' '[:lower:]')
-    python scripts/csv_2_tsv_net3.py \
-        $MONITORING_FOLDER/GRN_$method.csv \
-        "$MONITORING_FOLDER/matlab/"
-done
+# for method in "${methods[@]}"; do
+#     # method_lower=$(echo "$method" | tr '[:upper:]' '[:lower:]')
+#     python scripts/csv_2_tsv_net3.py \
+#         $MONITORING_FOLDER/GRN_$method.csv \
+#         "$MONITORING_FOLDER/matlab/"
+# done
 
-MATLAB_PREDICTIONS_FULL_PATH=$(realpath $MONITORING_FOLDER/matlab/)
+# MATLAB_PREDICTIONS_FULL_PATH=$(realpath $MONITORING_FOLDER/matlab/)
 
-cd /home/a62924/workspace/matlab/evaluation_scripts/matlab || exit 1;
+# cd /home/a62924/workspace/matlab/evaluation_scripts/matlab || exit 1;
 
-sudo matlab -nodesktop -nosplash -r "go_my('DimReduction', 64, '${MATLAB_PREDICTIONS_FULL_PATH}'); exit;" && \
+# sudo matlab -nodesktop -nosplash -r "go_my('DimReduction', 64, '${MATLAB_PREDICTIONS_FULL_PATH}'); exit;" && \
 
-for method in "${methods[@]}"; do
-    sudo matlab -nodesktop -nosplash -r "go_my('${method}', 64, '${MATLAB_PREDICTIONS_FULL_PATH}'); exit;"
-done
+# for method in "${methods[@]}"; do
+#     sudo matlab -nodesktop -nosplash -r "go_my('${method}', 64, '${MATLAB_PREDICTIONS_FULL_PATH}'); exit;"
+# done
 
-cp results/* $MATLAB_PREDICTIONS_FULL_PATH/results
+# cp results/* $MATLAB_PREDICTIONS_FULL_PATH/results
 
 cd - || exit 1;
 
@@ -85,6 +85,6 @@ mkdir -p $MONITORING_FOLDER/graphs
 python scripts/evaluation_analyzer_2.py \
     --time_file $MONITORING_FOLDER/evaluation_results_confident-in-0.csv \
     --data_folder $MONITORING_FOLDER/matlab/results \
-    --threads 1 \
+    --threads 64 \
     --output_dir $MONITORING_FOLDER/graphs
     
